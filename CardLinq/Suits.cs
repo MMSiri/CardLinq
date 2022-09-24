07a46a679a912a183056c0745c936ca5dcbbb7e5
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-09-24 17:05:56 +0100

    Add project files.

---
+++ CardLinq/Suits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardLinq
+{
+    enum Suits
+    {
+        Diamonds,
+        Clubs,
+        Hearts,
+        Spades,
+    }
+}
